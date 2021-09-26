96ccbb5ba8fc8e5950d40a56d14ee33edecc1a17
Jos3lu <jlsierrabenito@gmail.com>
2021-09-26 17:37:43 +0200

    Added frontend

---
+++ enjoymadrid-frontend/src/app/index/index.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.page.html',
+  styleUrls: ['./index.page.scss'],
+})
+export class IndexPage implements OnInit {
+
+  public id: string;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+  }
+
+}
