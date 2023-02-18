eb419acad3b20389f38fb17057fbc131c37cbe3a
Jos3lu <jlsierrabenito@gmail.com>
2023-02-18 15:41:50 +0100

    Updated Dictionary repository

--- enjoymadrid-backend/src/main/java/com/example/enjoymadrid/models/repositories/DictionaryRepository.java
+++ enjoymadrid-backend/src/main/java/com/example/enjoymadrid/models/repositories/DictionaryRepository.java
@@ -1,18 +1,13 @@
 package com.example.enjoymadrid.models.repositories;
 
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.enjoymadrid.models.Dictionary;
-import com.example.enjoymadrid.models.TouristicPoint;
 
 public interface DictionaryRepository extends JpaRepository<Dictionary, Long> {
-	
-	@Query("SELECT d FROM Dictionary d JOIN FETCH d.weights p WHERE KEY(p) = :touristicPoint")
-	Set<Dictionary> findByWeightsTouristicPoint(TouristicPoint touristicPoint);
-	
+		
 	Optional<Dictionary> findByTerm(String term);
+	
 }
